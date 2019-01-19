dca4d43deebdf21b57f98a881cd59679efcd104e
Guillaume-Levrier <Guillaume-Levrier@users.noreply.github.com>
2019-01-19 12:36:57 +0100

    Broken Gz
    - Tooltip is fixed
    - Brush kills the whole process

--- js/type/gazouillotype.js
+++ js/type/gazouillotype.js
@@ -3,26 +3,42 @@ const gazouillotype = (dataset,query) => {                          // When call
 //========== SVG VIEW =============
 var svg = d3.select(xtype).append("svg").attr("id","xtypeSVG");       // Creating the SVG node
 
-var keywordsTooltip = d3.select("#tooltip").append("div").attr("id","keywords");
-
-svg.attr("width", width-(0.3*width)).attr("height", height);          // Attributing width and height to svg
+svg.attr("width",width).attr("height", height);          // Attributing width and height to svg
 
 var view = svg.append("g")                                            // Appending a group to SVG
-              .attr("class", "view");                                 // CSS viewfinder properties
+              .attr("class", "view");
+
+var brushHeight = 150;
+
+var context = svg.append("g")
+    .attr("class", "context")
+    .attr("transform", "translate(0,"+(height-brushHeight-50)+")");
 
 var zoom = d3.zoom()
             .scaleExtent([0.6, 10])                                    // Extent to which one can zoom in or out
-            .translateExtent([[-200, -Infinity], [Infinity, height+100]])
+            .translateExtent([[-200, -Infinity], [Infinity, height+brushHeight]])
             .on("zoom", zoomed);                                     // Trigger the actual zooming function
 
-//========== X & Y AXIS  ============
-var x = d3.scaleTime().range([0,width]);
+var brush = d3.brushX()
+    .extent([[0, 0], [width, brushHeight-50]]);
+//    .on("brush end", brushed);
 
+//========== X & Y AXIS  ============
+var x = d3.scaleTime().range([0,width-(0.3*width)]),
+    x2 = d3.scaleTime().range([0,width-(0.3*width)]);
 var xAxis = d3.axisBottom(x).tickFormat(multiFormat);
 
-var y = d3.scaleLinear().range([height,0]);
+var y = d3.scaleLinear().range([height-brushHeight,0]),
+    y2 = d3.scaleLinear().range([brushHeight,0]);
 
 var yAxis = d3.axisRight(y);
+
+var area = d3.area()
+    .curve(d3.curveMonotoneX)
+    .x(d =>x2(d.timespan))
+    .y0(brushHeight)
+    .y1(d =>y2(d.indexPosition));
+
 
 //======== DATA CALL & SORT =========
 
@@ -67,19 +83,44 @@ const piler = () => {
 
 var firstDate = new Date(dataNest[0].key);
 var twoDaysLater = new Date(firstDate.getTime()+1.728e+8);
-
-
+var lastDate = new Date(dataNest[dataNest.length-1].key);
 
 x.domain([firstDate,twoDaysLater]);
 y.domain([0,200]);
 
-d3.select("#tooltip").html("<p>"+JSON.stringify(keywords)+"</p>");
+x2.domain([firstDate,lastDate]);
+y2.domain([0, d3.max(data, d=> d.indexPosition)]);
+
+
+const keywordsDisplay = () => {
+  document.getElementById("tooltip").innerHTML ="<p> Request content:<br> "+JSON.stringify(keywords)+"</p>";
+    };
+
+context.append("path")
+    .datum(data)
+    .style('fill','steelblue')
+    .attr("d", area);
+
+context.append("g")
+      .attr("class", "brush")
+      .attr("transform", "translate(0,"+(50)+")")
+      .call(brush)
+      .call(brush.move, x.range());
+
+/*
+svg.append("rect")                                   // Putting a rectangle above the grid to make dates easier to read
+    .attr("x", 0).attr("y", height-brushHeight)                       // Rectangle starts at top left
+    .attr("width", width).attr("height", brushHeight)         // Rectangle dimensions
+    .style("fill", "white")     // Rectangle background color and opacity
+    .style("stroke-width","0");                      // Invisible borders
+*/
 
 var circle = view.selectAll("circle")
               .data(data)
               .enter().append("circle")
+                  .attr("class", "circle")
                  .style('fill',d => color(d.retweet_count))
-                 .attr("r", d=> 1.5)
+                 .attr("r", d=> 1.3)
                  .attr("cx", d => x(d.timespan))
                  .attr("cy", d => y(d.indexPosition))
                    .on("mouseover", function(d) {d3.select(this).style("cursor", "pointer")})
@@ -124,7 +165,7 @@ var circle = view.selectAll("circle")
                          d.from_user_followercount+"<br> User friend count: "+
                          d.from_user_friendcount+"<br> User tweet count: "+
                          d.from_user_tweetcount+""+
-                       '<br><br><br><br><br><br><br><br><br><br>&nbsp;</p>')});
+                       '<br><br>Request content:<br> '+JSON.stringify(keywords)+'<br><br><br><br><br><br><br><br>&nbsp;</p>')});
 
 
 function narrative(focused) {                                                     // Experimental narrative function
@@ -144,6 +185,9 @@ d3.select("#option-icon").on("click", reset);           // Clicking the button "
 
 loadType();
 
+keywordsDisplay();
+
+x.domain([firstDate,lastDate]);
 
 });  //======== END OF DATA CALL (PROMISES) ===========
 
@@ -156,16 +200,39 @@ var gX = svg.append("g")                                          // Make X axis
 
 var gY = svg.append("g")                                          // Make Y axis rescalable
             .attr("class", "axis axis--y")
-            .attr("transform", "translate(" + (width -420 )+ ",0)")
+            .attr("transform", "translate(" + (width -(0.3*width)-70 )+ ",0)")
             .call(yAxis);
 
 svg.call(zoom).on("dblclick.zoom", null);                         // Zoom and deactivate doubleclick zooming
 
 function zoomed() {
+  if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
+  var t = d3.event.transform;
+  //view.select(".circle").attr("d", circle);
+
+  context.select(".brush").call(brush.move, x2.range().map(t.invertX, t));
    view.attr("transform", d3.event.transform);
      gX.call(xAxis.scale(d3.event.transform.rescaleX(x)));
      gY.call(yAxis.scale(d3.event.transform.rescaleY(y)));
+  //   x.domain(t.rescaleX(x2).domain());
    }
 
+function brushed() {
+  if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
+  //var s = d3.event.selection || x2.range();
+  //x.domain(s.map(x2.invert, x2));
+  //view.select(".circle").attr("d", circle);
+  //gX.call(xAxis.scale(d3.event.transform.rescaleX(x)));
+  //view.call(zoom.transform, d3.zoomIdentity
+  //    .scale(width / (s[1] - s[0]))
+  //    .translate(-s[0], 0));
+}
+
+function type(d) {
+  d.date = new Date(d.created_at);
+  d.timespan = new Date(Math.round(d.date.getTime()/600000)*600000);
+  d.indexPosition = +d.indexPosition;
+  return d;
+}
 ipcRenderer.send('console-logs',"Starting gazouillotype");           // Starting gazouillotype
 }                                                                 // Close gazouillotype function
