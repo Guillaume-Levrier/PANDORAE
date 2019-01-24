00f9305aeb0bac40f3077ba8e8373c05abab0041
Guillaume LEVRIER <guillaume.levrier@sciencespo.fr>
2019-01-24 14:44:13 +0100

    Doesn't scroll to relevant section yet

--- js/tutorial/tutorial.js
+++ js/tutorial/tutorial.js
@@ -2,6 +2,14 @@ const {ipcRenderer} = require('electron');                       // ipcRenderer 
 
 ipcRenderer.send("window-ids","tutorial",remote.getCurrentWindow().id);
 
+ipcRenderer.on('window-close', (event,message) => {
+  closeWindow();
+});
+
+ipcRenderer.on('scroll-to', (event,message) => {
+  smoothScrollTo(message)
+});
+
 const tutoFlux = () => {
   ipcRenderer.send('tutorial','flux');
   closeWindow();
