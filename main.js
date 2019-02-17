0d62ff576e0a94ebf69891585d582dcf356befb1
Guillaume-Levrier <Guillaume-Levrier@users.noreply.github.com>
2019-02-17 15:36:43 +0100

    first theme !

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function createWindow () {
      backgroundColor: 'white',
      titleBarStyle: 'hidden',
      frame: true,
+     resizable:false,
      webPreferences: {
          nodeIntegrationInWorker: true
        }
