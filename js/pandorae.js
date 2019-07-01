df75fa27f9c18aa1c5c57ff53b66d0481fce0664
Guillaume-Levrier <guillaume.levrier@sciencespo.fr>
2019-07-01 17:36:51 +0200

    version update

--- js/pandorae.js
+++ js/pandorae.js
@@ -13,7 +13,7 @@
 //
 // ============ VERSION ===========
 const msg = '      ______\n     / _____|\n    /  ∖____  Anthropos\n   / /∖  ___|     Ecosystems\n  / /  ∖ ∖__\n /_/    ∖___|           PANDORÆ\n\n';
-const version ='BETA/DEV-V0.1.23';
+const version ='BETA/DEV-V0.1.29';
 console.log(msg+version);
 
 // =========== NODE - NPM ===========
