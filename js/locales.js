dd13276b36468e1ca54b5305f07e21acf8dd8a22
Guillaume LEVRIER <guillaume.levrier@sciencespo.fr>
2019-12-11 10:31:18 +0100

    first steps in localization

---
+++ js/locales.js
@@ -0,0 +1,46 @@
+const CMT = {"EN":{
+        "global":{
+            "field":{
+            "starting":"starting",
+            "error": "system error",
+            "workerError":"multithreading disabled"
+            },
+            "console":{
+                "starting":["Starting process "," using dataset "],
+                "error":["SYSTEM ERROR - PANDORÆ was unable to perform this task. The dataset might be incompatible with your request."],
+                "workerError":["The SharedWorker failed to start. Processes requiring multithreading will be unavailable."],
+                "workerValidation":["Multithreading enabled."],
+                "menu":{
+                    "opening":"Opening menu",
+                    "closing":"Collapsing menu"
+                }
+        },
+        "chaeros":{ 
+            "field":{
+
+            },
+            "console":{
+
+        }},
+        "flux":{ 
+            "field":{
+
+        },
+        "console":{
+
+        }},
+        "types":{ 
+            "field":{
+
+        },
+        "console":{
+
+        }}
+
+        ,
+    "tutorial":{
+
+        }
+    }
+}
+}
