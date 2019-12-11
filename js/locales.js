5a08efa594d4f242b53864036eb5ebb339ff2658
Guillaume-Levrier <guillaume.levrier@sciencespo.fr>
2019-12-11 18:36:46 +0100

    tutorial needs fixing

--- js/locales.js
+++ js/locales.js
@@ -11,7 +11,8 @@ const CMT = {"EN":{
             "starting":"starting",
             "error": "system error",
             "workerError":"multithreading disabled"
-            },
+            }
+        },
             "console":{
                 "starting":["Starting process "," using dataset "],
                 "error":["SYSTEM ERROR - PANDORÆ was unable to perform this task. The dataset might be incompatible with your request."],
@@ -46,8 +47,14 @@ const CMT = {"EN":{
 
         ,
     "tutorial":{
+        "sections":{
+            "s1":"Welcome to PANDORÆ<br>&nbsp;<br>&nbsp;<br>",
+            "s2":"<span class='title'>This brief tutorial will show you what PANDORÆ does.</span>",
+            "s3":"<span class='title'>As you noticed, you can click on the <strong>arrows</strong> to go down or back.</span><br>You can also use your <strong>mouse wheel</strong> to navigate up and down.&nbsp;<br><br>            You can skip specific chapters and come back to this <strong>tutorial</strong> later.<br><br>            If you're unsure of how to proceed, you can watch the step-by-step video of this tutorial while you interact with it."
 
         }
-    }
+
+        }
+    
 }
 }
---
+++ js/tutorial/textcontent.js
@@ -0,0 +1,5 @@
+var CM = CMT["EN"];
+
+document.getElementById("s1").innerHTML = CM.tutorial.sections.s1;
+document.getElementById("s2").innerHTML = CM.tutorial.sections.s2;
+document.getElementById("s3").innerHTML = CM.tutorial.sections.s3;
