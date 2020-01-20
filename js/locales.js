e9c02b87b2d7ff3627069825db94d71256681bd3
Guillaume-Levrier <Guillaume-Levrier@users.noreply.github.com>
2020-01-20 15:45:59 -0800

    Tutorial fix

--- js/locales.js
+++ js/locales.js
@@ -46,7 +46,10 @@ const CMT = {"EN":{
             "console":{
 
         }},
-        "flux":{ 
+        "flux":{
+            "divs":{
+                "hyphendpoint":"Enter your Hyphe endpoint below :"
+            }, 
             "field":{
 
         },
@@ -71,7 +74,28 @@ const CMT = {"EN":{
 
         }
     
-},"FR":{
+},
+"FR":{
+    "menu":{
+        "flux":"flux",
+        "type":"type",
+        "quit":"quitter",
+        "returnToTutorial":"retour au tutoriel"
+    },
 
-}
+    "tutorial":{
+        "sections":{
+            "s1":"Bienvenue sur PANDORAE<br>&nbsp;<br>&nbsp;<br>",
+            "s2":"<span class='title'>Ce bref tutoriel a pour but de vous montrer ce que peut faire PANDORÆ.</span>",
+            "s3":"<span class='title'>Comme vous vous en êtes aperçu, les <strong>flêches</strong> to go down or back.</span><br>You can also use your <strong>mouse wheel</strong> to navigate up and down.&nbsp;<br><br>            You can skip specific chapters and come back to this <strong>tutorial</strong> later.<br><br>            If you're unsure of how to proceed, you can watch the step-by-step video of this tutorial while you interact with it."
+        }
+    }
+ },"中文":{
+    "menu":{
+        "flux":"流动",
+        "type":"显示",
+        "quit":"关闭",
+        "returnToTutorial":"回教程"
+    }
+ }
 }
--- js/pandorae.js
+++ js/pandorae.js
@@ -51,9 +51,28 @@ fs.readFile(userDataPath + "/userID/user-id.json", "utf8",     // Check if the u
         CM = CMT[data.locale]
       })
 
+const populateLocale = divlist => {
+
+   divlist.forEach(div=>{
+    document.getElementById(div.id).innerText=CM.menu[div.path];
+  }) 
+
+}
+
+var divlist = [
+  {id:"fluxMenu",path:"flux"},
+  {id:"type",path:"type"},
+  {id:"quitBut",path:"quit"},
+  {id:"tutostartmenu",path:"returnToTutorial"}
+]
+
+populateLocale(divlist)
+
+
 document.getElementById("lang").childNodes.forEach(lg=>{
   lg.addEventListener("click",e=>{
     CM=CMT[lg.innerText];
+    populateLocale(divlist)
     fs.readFile(userDataPath + "/userID/user-id.json", "utf8",
       (err, data) => {
         data = JSON.parse(data);
@@ -181,10 +200,6 @@ let iconDiv = document.getElementById("icons");
 let menu = document.getElementById("menu");
 let consoleDiv =document.getElementById("console")
 
-document.getElementById("fluxMenu").innerText=CM.menu.flux;
-document.getElementById("type").innerText=CM.menu.type;
-document.getElementById("quitBut").innerText=CM.menu.quit;
-document.getElementById("tutostartmenu").innerText=CM.menu.returnToTutorial;
 
 // Menu behaviors
 let toggledMenu = false;
