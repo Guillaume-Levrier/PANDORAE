1091d432c476780b23b563435a5dcf9a1a52beab
Guillaume-Levrier <Guillaume-Levrier@users.noreply.github.com>
2020-10-07 11:26:03 +0200

    better window management from main

--- js/tutorial/tutorial.js
+++ js/tutorial/tutorial.js
@@ -1,15 +1,9 @@
 const { ipcRenderer } = require("electron"); // ipcRenderer manages messages with Main Process
 
-
-ipcRenderer.on("window-close", (event, message) => {  
-  closeWindow();
-});
-
 ipcRenderer.on("scroll-to", (event, message) => {
   smoothScrollTo(message);
 });
 
 const tuto = step => {
   ipcRenderer.send("tutorial", step);
-  closeWindow();
 };
