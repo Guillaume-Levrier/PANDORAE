d71803ead5123688045b0ad2cc42377e54dda152
Guillaume-Levrier <guillaume.levrier@sciencespo.fr>
2021-04-18 17:48:44 +0200

    regards as d3 hierarchy

--- js/preload-audio.js
+++ js/preload-audio.js
@@ -1,3 +1,3 @@
 const { ipcRenderer } = require("electron");
 
-ipcRenderer.on("audio-channel",(event, audio) => document.getElementById(audio).play());
+//ipcRenderer.on("audio-channel",(event, audio) => document.getElementById(audio).play());
--- js/types.js
+++ js/types.js
@@ -2089,11 +2089,205 @@ const hyphotype = (id) => {
 const regards = (id) => {
   // When called, draw the chronotype
 
-  console.log(id);
+  var svg = d3.select(xtype).append("svg").attr("id", "xtypeSVG");
 
-  pandodb.regards.get(id).then((datajson) => {
-    console.log(datajson);
-  });
+  svg
+    .attr("width", width - toolWidth)
+    .attr("height", height) // Attributing width and height to svg
+    .attr("viewBox", [
+      -(width - toolWidth) / 2,
+      -height / 2,
+      width - toolWidth,
+      height,
+    ]);
+
+  var view = svg
+    .append("g") // Appending a group to SVG
+    .attr("id", "view"); // CSS viewfinder properties
+
+  //console.log(id);
+
+  zoom
+    .scaleExtent([0.1, 20]) // Extent to which one can zoom in or out
+    .translateExtent([
+      [-Infinity, -Infinity],
+      [Infinity, Infinity],
+    ]) // Extent to which one can go up/down/left/right
+    .on("zoom", ({ transform }, e) => {
+      zoomed(transform);
+    });
+
+  pandodb.regards
+    .get(id)
+    .then((datajson) => {
+      dataDownload(datajson);
+
+      let chronoData = {};
+
+      for (const key in datajson.content) {
+        datajson.content[key].forEach((val) => {
+          if (key != "seances" && key != "texteloi") {
+            let date = val.content.date;
+            if (chronoData.hasOwnProperty(date)) {
+            } else {
+              chronoData[date] = {};
+            }
+            if (chronoData[date].hasOwnProperty(key)) {
+            } else {
+              chronoData[date][key] = [];
+            }
+            chronoData[date][key].push(val);
+          }
+        });
+      }
+
+      const data = { name: "regards", children: [] };
+
+      for (const key in chronoData) {
+        let child = { name: key, date: key, children: [] };
+        for (const prop in chronoData[key]) {
+          chronoData[key][prop].forEach((d) => {
+            if (d.content.hasOwnProperty("sujet")) {
+              d.name =
+                d.content.signataires +
+                " | " +
+                d.content.sujet +
+                " | " +
+                d.content.id;
+            } else if (d.content.hasOwnProperty("intervenant_nom")) {
+              d.name = d.content["intervenant_nom"] + " | " + d.content.id;
+            }
+          });
+
+          chronoData[key][prop].sort((a, b) => a.content.id - b.content.id);
+
+          child.children.push({ name: prop, children: chronoData[key][prop] });
+        }
+        data.children.push(child);
+      }
+
+      const tree = (data) => {
+        const root = d3
+          .hierarchy(data)
+          .sort(
+            (a, b) =>
+              d3.ascending(a.data.date, b.data.date) ||
+              d3.ascending(a.document_id, b.document_id)
+          );
+        root.dx = 20;
+        root.dy = width / (root.height + 3);
+        return d3.cluster().nodeSize([root.dx, root.dy])(root);
+      };
+
+      const root = tree(data);
+
+      const link = view
+        .append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#555")
+        .attr("stroke-opacity", 0.4)
+        .attr("stroke-width", 1.5)
+        .selectAll("path")
+        .data(root.links())
+        .join("path")
+        .style("display", (d) => (d.source.depth === 0 ? "none" : "block"))
+        .attr(
+          "d",
+          (d) => `
+        M${d.target.y},${d.target.x}
+        C${d.source.y + root.dy / 2},${d.target.x}
+         ${d.source.y + root.dy / 2},${d.source.x}
+         ${d.source.y},${d.source.x}
+      `
+        );
+
+      const node = view
+        .append("g")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-width", 3)
+        .selectAll("g")
+        .data(root.descendants())
+        .join("g")
+        .attr("transform", (d) => `translate(${d.y},${d.x})`);
+
+      node
+        .append("circle")
+        .style("display", (d) => (d.depth === 0 ? "none" : "block"))
+        .attr("fill", (d) => (d.children ? "#555" : "#999"))
+        .attr("r", 2.5);
+
+      node
+        .append("text")
+        .attr("dy", "0.31em")
+        .style("font-size", "12px")
+        .style("cursor", (d) => (d.depth === 3 ? "pointer" : "auto"))
+        .attr("x", (d) => (d.children ? -6 : 6))
+        .text((d) => d.data.name)
+        .on("click", (event, d) => {
+          if (d.depth === 3) {
+            dt = d.data.content;
+            if (dt.hasOwnProperty("contenu")) {
+              tooltip.innerHTML = dt.contenu;
+            } else if (dt.hasOwnProperty("expose")) {
+              tooltip.innerHTML = dt.expose + "<br><br>" + dt.texte;
+            }
+          }
+        })
+        .style("display", (d) => (d.depth === 0 ? "none" : "block"))
+        .filter((d) => d.children)
+        .attr("text-anchor", "end")
+        .clone(true)
+        .lower()
+        .style("display", (d) => (d.depth === 0 ? "none" : "block"))
+        .attr("stroke", "white");
+
+      let timeline = {
+        x1: root.children[0].y,
+        y1: root.children[0].x,
+        x2: root.children[root.children.length - 1].y,
+        y2: root.children[root.children.length - 1].x,
+      };
+
+      var tl = view
+        .append("line")
+        .attr("stroke", "#555")
+        .attr("stroke-opacity", 0.4)
+        .attr("stroke-width", 1.5)
+        .attr("x1", timeline.x1)
+        .attr("x2", timeline.x2)
+        .attr("y1", timeline.y1)
+        .attr("y2", timeline.y2)
+        .lower();
+
+      loadType();
+
+      view.attr("transform", "translate(" + -width / 3 + ",0)");
+    })
+    .catch((error) => {
+      field.value = "error - invalid dataset";
+      ipcRenderer.send(
+        "console-logs",
+        "Chronotype error: dataset " + id + " is invalid."
+      );
+    });
+  let dragger = svg
+    .append("rect")
+    .attr("x", -width)
+    .attr("y", -height)
+    .attr("width", width * 2)
+    .attr("height", height * 2)
+    .attr("fill", "white")
+    .style("cursor", "all-scroll")
+    .lower();
+
+  dragger.call(zoom).on("dblclick.zoom", null); // Zoom and deactivate doubleclick zooming
+  //.on("mousedown.zoom", d=>{if(brushing){return null}})
+
+  zoomed = (thatZoom) => {
+    //thatZoom.x=0;
+    //thatZoom.y=0,
+    view.attr("transform", thatZoom);
+  };
 };
 
 // ========== CHRONOTYPE ==========
