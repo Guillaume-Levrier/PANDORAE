fcd30ea4f3f2ce0674e670e87f8f15f5e9b2e677
Guillaume Levrier <Guillaume-Levrier>
2025-02-18 15:04:36 +0100

    changing config to enable packaging

--- forge.config.js
+++ forge.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     {
       name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
+      platforms: ["darwin", "linux"],
     },
     {
       name: "@electron-forge/maker-deb",
